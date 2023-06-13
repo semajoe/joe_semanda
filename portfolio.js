7e9c59bfa373b0ccc8feb7e1967127b2e9171687
Semanda Joe <44225247+semajoe@users.noreply.github.com>
2023-06-13 15:34:40 +0100

    google sheets

--- portfolio.js
+++ portfolio.js
@@ -18,7 +18,25 @@ var menus = document.getElementById('side-menu')
 
 function openmenu(){
     menus.style.right = '0';
-}
+}  
 function closemenu(){
     menus.style.right = '-200px';
 }
+
+// Google Sheets
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx22Zfryo7rp8QzsUbLKGLBTZUUZqMFDdmp8V23u8DUg3YWSM7ti2Hz9_qaJF-TYBkt/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById('msg')
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = 'Message Sent!'
+        setTimeout(function(){
+            msg.innerHTML = ''
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  }) 
