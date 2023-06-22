3a24be6b99131761efbf8de512a7df73f04a4930
Semanda Joe <44225247+semajoe@users.noreply.github.com>
2023-06-22 17:16:00 +0100

    greetings in javascript

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,22 @@
+// Greetings
+var today = new Date();
+var hourNow = today.getHours();
+var greetings;
+
+if (hourNow > 18) {
+    greetings = 'Good Evening!';
+} else if(hourNow >12){
+    greetings = 'Good Afternoon!';
+} else if(hourNow > 0){
+    greetings = 'Good Morning!';
+} else{
+    greetings = 'Welcome!';
+};
+
+var elGreeting = document.getElementById('greetings');
+elGreeting.innerHTML = greetings;
+
+
 var tablinks = document.getElementsByClassName('tab-links');
 var tabcontents = document.getElementsByClassName('tab-contents');
 
